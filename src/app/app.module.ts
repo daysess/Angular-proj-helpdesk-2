0f119c3c9901593bf6306b1684e74799427e0141
daysess <daysedesign@gmail.com>
2022-04-11 20:52:38 -0300

    Interceptor para requisicoes http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptiorProvider } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -73,7 +74,7 @@ import { ToastrModule } from 'ngx-toastr';
     })
     
   ],
-  providers: [],
+  providers: [AuthInterceptiorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

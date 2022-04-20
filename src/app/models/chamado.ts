edd1190c98a4ea038d606858d4bb917258363015
daysess <daysedesign@gmail.com>
2022-04-19 21:33:23 -0300

    Servico para criar novo chamado implementado

--- src/app/models/chamado.ts
+++ src/app/models/chamado.ts
@@ -1,13 +1,13 @@
 export interface Chamado {
-	id?:                any;
-	dataAbertura?:      string;
-	dataFechamento?:    string;
-	idPrioridade:         string;
-	idStatus:           string;
-	titulo:             string;
-	descricao:          string;
-	tecnico:            any;
-	cliente:            any;
-	nomeCliente:        string;
-	nomeTecnico:        string;
+	id?:                	any;
+	dataAbertura?:      	string;
+	dataFechamento?:    	string;
+	idPrioridade:         	string;
+	idStatus:           	string;
+	titulo:             	string;
+	observacoes:          	string;
+	idTecnico:            	any;
+	idCliente:            	any;
+	nomeCliente:        	string;
+	nomeTecnico:        	string;
 }
